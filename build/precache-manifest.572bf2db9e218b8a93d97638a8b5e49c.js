f664f8608288bb6a79e0177ee643768a45cfd58a
Jay Bhatt <bhatjay897@gmail.com>
2020-04-09 01:54:42 +0530

    including build to show on webpage

---
+++ build/precache-manifest.572bf2db9e218b8a93d97638a8b5e49c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b34b34ffc6bfff3235069ce2d096320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9efd03ef81e1628b9f94",
+    "url": "/static/css/main.83ba6b31.chunk.css"
+  },
+  {
+    "revision": "a34390d7091d02234b98",
+    "url": "/static/js/2.8c1add18.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8c1add18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9efd03ef81e1628b9f94",
+    "url": "/static/js/main.211ac4be.chunk.js"
+  },
+  {
+    "revision": "389153c2b4a5ac0ff24a",
+    "url": "/static/js/runtime-main.cbb4d1cf.js"
+  },
+  {
+    "revision": "4a39962b5306eef27e249d24de3c6364",
+    "url": "/static/media/Adventure.4a39962b.svg"
+  },
+  {
+    "revision": "650ced7492e9b4da4e473cdfde0e7aab",
+    "url": "/static/media/Back.650ced74.svg"
+  },
+  {
+    "revision": "f737ae03b366ed791d2b6dd0fc20e0bc",
+    "url": "/static/media/Cancel.f737ae03.svg"
+  },
+  {
+    "revision": "ffe5b2615b910b0a79cc8011ee7ef964",
+    "url": "/static/media/Drama.ffe5b261.svg"
+  },
+  {
+    "revision": "fb8af2708ea38a143a34b8f248b57463",
+    "url": "/static/media/Fiction.fb8af270.svg"
+  },
+  {
+    "revision": "0689e2d1c23d48e1c0206075d121bd3f",
+    "url": "/static/media/History.0689e2d1.svg"
+  },
+  {
+    "revision": "5218701c2d69690ae889cfec09c26176",
+    "url": "/static/media/Humour.5218701c.svg"
+  },
+  {
+    "revision": "1b38414956c666bd1df78fe5b9c84756",
+    "url": "/static/media/Montserrat-BoldItalic.1b384149.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "aa84e2745e1657cb1aed779f4485641a",
+    "url": "/static/media/Next.aa84e274.svg"
+  },
+  {
+    "revision": "10f528b0a49efbd5e38d5f518f667a24",
+    "url": "/static/media/Pattern.10f528b0.svg"
+  },
+  {
+    "revision": "faf4e2b34170f89e2704da510960d46c",
+    "url": "/static/media/Philosophy.faf4e2b3.svg"
+  },
+  {
+    "revision": "bb1d383211ac28a293afbe9619c2dade",
+    "url": "/static/media/Politics.bb1d3832.svg"
+  },
+  {
+    "revision": "8492e7a8993b7335d094584dba64a38b",
+    "url": "/static/media/Search.8492e7a8.svg"
+  },
+  {
+    "revision": "34f67de5d1cfd0cb77ffa99820ab815a",
+    "url": "/static/media/loader_animated.34f67de5.svg"
+  },
+  {
+    "revision": "090df0dc2642b738b456c85d717cab33",
+    "url": "/static/media/loader_static.090df0dc.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
